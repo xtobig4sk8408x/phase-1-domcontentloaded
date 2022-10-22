962d095e3b02908eb3d8e8ba61d2629f5bd7b7c9
Theodore John Josef <john.josef@aol.com>
2022-10-22 01:30:43 -0700

    commit tree 5a8e7208cba10ca106a638dd03010b2e4145b049

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const phrase = document.querySelector("p");
+    phrase.textContent = "This is really cool!";
+});
